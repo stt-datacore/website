b13889009d1cfd6e2d94fad3005b8ce91feb84f0
TemporalAgent7 <alejandro93@protonmail.com>
2019-03-16 00:23:12 -0700

    Add more gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,8 @@ module.exports = {
 				name: 'pages',
 				path: `${__dirname}/static/pages/`
 			}
-        },
-        {
+		},
+		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				name: 'crew',
@@ -28,12 +28,32 @@ module.exports = {
 				name: 'structured',
 				path: `${__dirname}/static/structured/`
 			}
-		}
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Star Trek Timelines DataCore`,
+				short_name: `DataCore`,
+				start_url: `/`,
+				background_color: `#3A3F44`,
+				theme_color: `#272B30`,
+				display: `standalone`,
+				icon: `src/images/logo.svg`,
+				include_favicon: true
+			}
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-112738113-3'
+			}
+		},
+		`gatsby-plugin-netlify`
 	],
 	mapping: {
-		"CollectionsJson.crew": `CrewJson.symbol`,
-		"CrewJson.equipment_slots.symbol": `ItemsJson.symbol`,
-		"ItemsJson.item_sources.id": `QuestsJson.id`,
-		"ItemsJson.recipe.list.symbol": `ItemsJson.symbol`,
-	  }
+		'CollectionsJson.crew': `CrewJson.symbol`,
+		'CrewJson.equipment_slots.symbol': `ItemsJson.symbol`,
+		'ItemsJson.item_sources.id': `QuestsJson.id`,
+		'ItemsJson.recipe.list.symbol': `ItemsJson.symbol`
+	}
 };
