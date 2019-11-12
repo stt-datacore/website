adbb0c20e3b3b55ae7af3fc4815e0b26375e2469
TemporalAgent7 <alejandro93@protonmail.com>
2019-11-12 15:27:41 -0800

    Add redirect notice

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,30 @@
-import React, { PureComponent } from 'react';
-import TopMenu from './topmenu';
+import React, { PureComponent } from "react";
+import { Message } from "semantic-ui-react";
+import TopMenu from "./topmenu";
 
 class Layout extends PureComponent {
-	render() {
-		return (
-			<React.StrictMode>
-				<div>
-					<TopMenu />
-					{this.props.children}
-				</div>
-			</React.StrictMode>
-		);
-	}
+  render() {
+    let redirectMessage = window.location.hostname.indexOf("netlify") > 0;
+    return (
+      <React.StrictMode>
+        <div>
+          <TopMenu />
+          {redirectMessage && (
+            <Message warning style={{ marginTop: "3.5em" }}>
+              <Message.Header>New website address!</Message.Header>
+              <p>
+                DataCore moved to a new home at{" "}
+                <a href="https://datacore.app">datacore.app</a>. Please update
+                your bookmarks, the old site will be shut down by the end of
+                November 2019.
+              </p>
+            </Message>
+          )}
+          {this.props.children}
+        </div>
+      </React.StrictMode>
+    );
+  }
 }
 
 export default Layout;
