6ed315b5c5fc18f31cac1e0a861d59f6f7067e4b
AlexCPU <alexcpu@gmail.com>
2020-08-22 09:15:44 +0000

    update publish script to check remote repo before rebuilding, can be run from cron now

--- publish.sh
+++ publish.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
-GIT_PATH=/home/stt/datacore
+#GIT_PATH=/home/stt/datacore
+GIT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pushd $GIT_PATH
+
+if [[ -f ".git/index.lock" ]]; then
+    echo "ERROR: Git repository is locked, waiting to unlock." >&2
+    sleep 5
+    continue
+fi
+
+git fetch
+
+repo_name=$(basename -s .git "$(git config --get remote.origin.url)")
+
+upstream="$(git rev-parse --abbrev-ref --symbolic-full-name "@{u}" 2>/dev/null)"
+
+# upstream was not configured
+if [[ -z "$upstream" ]]; then
+    echo "ERROR: Git repository does not have an upstream source."
+    exit 1
+fi
+
+git_local=$(git rev-parse @)
+git_remote=$(git rev-parse "$upstream")
+git_base=$(git merge-base @ "$upstream")
+
+
+if [[ "$git_local" == "$git_remote" ]]; then
+    echo "No update to pull."
+    exit 1
+elif [[ "$git_local" == "$git_base" ]]; then
+    echo "Update found, pulling update and rebuilding website."
+elif [[ "$git_remote" == "$git_base" ]]; then
+    echo "ERROR: Local git repo is ahead of remote.  You may need to push or revert changes."
+    exit 1
+else
+    echo "ERROR: Local git repo has diverged from remote. Unable to fix."
+    exit 1
+fi
+
 # This would be more thorough but would also require re-installing all dependencies in node_modules
 #git clean -x
 rm -rf ./public/
