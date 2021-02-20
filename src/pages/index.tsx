d2722f5b1c7d7a3f8bf76de95c448765e4cdd4a1
joshurtree <joshurtree@gmail.com>
2021-02-20 23:47:06 +0000

    Create different search levels

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Table, Rating, Icon } from 'semantic-ui-react';
+import { Container, Header, Table, Rating, Icon, Button } from 'semantic-ui-react';
 import { navigate } from 'gatsby';
 
 import Layout from '../components/layout';
@@ -14,7 +14,7 @@ type IndexPageState = {
 };
 
 const tableConfig: ITableConfigRow[] = [
-	{ width: 3, column: 'name', title: 'Crew' },
+	{ width: 3, column: 'name', title: 'Crew', pseudocolumns: ['name', 'bigbook_tier', 'events'] },
 	{ width: 1, column: 'max_rarity', title: 'Rarity' },
 	{ width: 1, column: 'command_skill', title: 'Command' },
 	{ width: 1, column: 'science_skill', title: 'Science' },
@@ -25,8 +25,13 @@ const tableConfig: ITableConfigRow[] = [
 ];
 
 class IndexPage extends Component<IndexPageProps, IndexPageState> {
-	state = { botcrew: [] };
-
+	state = { botcrew: [], searchLevel : 2 };
+    searchTypes = [
+        (input: string, searchString: string) => input.toLowerCase() == searchString.toLowerCase(),
+        (input: string, searchString: string) => new RegExp('\\b' + searchString + '\\b', 'i').test(input),
+        (input: string, searchString: string) => input.toLowerCase().indexOf(searchString.toLowerCase()) >= 0
+    ];
+    
 	async componentDidMount() {
 		let response = await fetch('/structured/crew.json');
 		const botcrew = await response.json();
@@ -42,8 +47,7 @@ class IndexPage extends Component<IndexPageProps, IndexPageState> {
 	}
 
 	_filterCrew(crew: any, filters: []): boolean {
-		const matchesFilter = (input: string, searchString: string) => input.toLowerCase().indexOf(searchString.toLowerCase()) >= 0;
-
+		const matchesFilter = this.searchTypes[this.state.searchLevel];
 		let meetsAnyCondition = false;
 
 		for (let filter of filters) {
@@ -53,6 +57,7 @@ class IndexPage extends Component<IndexPageProps, IndexPageState> {
 				for (let segment of filter.textSegments) {
 					let segmentResult =
 						matchesFilter(crew.name, segment.text) ||
+						matchesFilter(crew.short_name, segment.text) ||
 						crew.traits_named.some(t => matchesFilter(t, segment.text)) ||
 						crew.traits_hidden.some(t => matchesFilter(t, segment.text));
 					meetsAllConditions = meetsAllConditions && (segment.negated ? !segmentResult : segmentResult);
@@ -79,6 +84,8 @@ class IndexPage extends Component<IndexPageProps, IndexPageState> {
 						let skillShort = CONFIG.SKILLS_SHORT.find(skill => skill.short === condition.value.toUpperCase());
 						let skillName = skillShort ? skillShort.name : condition.value.toLowerCase()+"_skill";
 						conditionResult = skillName in crew.base_skills;
+					} else if (condition.keyword === 'in_portal') {
+						conditionResult = condition.value.toLowerCase() === 'true' ? crew.in_portal : !crew.in_portal;
 					}
 					meetsAllConditions = meetsAllConditions && (condition.negated ? !conditionResult : conditionResult);
 				}
@@ -187,12 +194,31 @@ class IndexPage extends Component<IndexPageProps, IndexPageState> {
 								<p>
 									<code>trait:female rarity:4,5 skill:sci trait:"q continuum"</code>
 								</p>
+
+								<p>
+									Search for all crew that are in the game portal (<b>true</b>) or not (any other value):
+								</p>
+								<p>
+									<code>in_portal:true</code>
+								</p>
 							</div>
 						}
 						renderTableRow={crew => this.renderTableRow(crew)}
 						filterRow={(crew, filter) => this._filterCrew(crew, filter)}
 						config={tableConfig}
-					/>
+                        footerExt = {() => {
+                            return (
+                            <span>
+                                {this.state.searchLevel > 0 && <Button onClick={() => this.setState({searchLevel : this.state.searchLevel - 1})}>
+                                    Show less
+                                </Button>}
+                                {this.state.searchLevel < this.searchTypes.length - 1 && <Button onClick={() => this.setState({searchLevel : this.state.searchLevel + 1})}>
+                                    Show more
+                                </Button>}
+                            </span>
+                            );
+                        }}
+                    />
 
 					<p>
 						<i>Hint</i> Click on a row to get details on that specific crew
