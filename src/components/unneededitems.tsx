4f67856ddb02f44a35088d58b4053435d6f55753
ussjohnjay <76980548+ussjohnjay@users.noreply.github.com>
2021-02-20 16:44:38 -0600

    Unneeded Items added tier, bug fixes
    Fixed a couple of bugs that I missed when moving this to its own component:
    1) Non-crew-specific items are considered properly again
    2) Item limit is shown again. Note that this uses a hardcoded 1000, which works fine now but may need to be changed if the game ever increases the inventory limit. There's a player data variable that we previously used, but it's no longer available when we switched to using preparedProfileData on tools components.
    
    After looking at the code, the previous logic was a little zealous in identifying unneeded items, so I've tweaked it a little to be more conservative, even if that means misses some potential unneeded items.
    
    Additionally, I added another tier so that unneeded items are presented as 1) unneeded schematics, 2) unneeded crew-specific items, and 3) unneeded generic items that could be useful later.
    
    This fixes #21 and #46 (Pike's Enterprise schematics were incorrectly identified as crew-specific) and #73 (crew-specific items were being identified for non-immortalized crew). This also properly checks for items whose names use fancy apostrophes.

--- src/components/unneededitems.tsx
+++ src/components/unneededitems.tsx
@@ -11,8 +11,9 @@ type UnneededItemsProps = {
 };
 
 type UnneededItemsState = {
-	fuellist: any[];
-	fuelschematicslist: any[];
+	fuelschematics: any[];
+	fuelspecific: any[];
+	fuelgeneric: any[];
 };
 
 class UnneededItems extends Component<UnneededItemsProps, UnneededItemsState> {
@@ -20,8 +21,9 @@ class UnneededItems extends Component<UnneededItemsProps, UnneededItemsState> {
 		super(props);
 
 		this.state = {
-			fuellist: [],
-			fuelschematicslist: []
+			fuelschematics: [],
+			fuelspecific: [],
+			fuelgeneric: []
 		};
 	}
 
@@ -39,20 +41,15 @@ class UnneededItems extends Component<UnneededItemsProps, UnneededItemsState> {
 		let items = mergeItems(playerData.player.character.items, allitems);
 		let ships = mergeShips(allships, playerData.player.character.ships);
 
-		let equipmentAlreadyOnCrew = new Set();
+		//let testitem = items.find(item => item.name === "Salia’s True Form" && item.rarity === 5);
+		//if (testitem)
+		//	console.log(testitem);
 
-		let fuellist = items.filter(
-			item =>
-				(equipmentAlreadyOnCrew.has(item.symbol) && item.quantity === 1 && item.rarity > 1) ||
-				item.name.indexOf("'s ") > 0 ||
-				item.name.indexOf("s' ") > 0
-		);
-
+		// Calculate unneeded schematics
 		let maxedShips = ships.filter(
 			ship => ship.level === ship.max_level
 		);
-
-		let fuelschematicslist = items.filter(
+		let fuelschematics = items.filter(
 			item => maxedShips.some((ship) => {
 				if (ship.symbol+'_schematic' === item.symbol) {
 					item.rarity = ship.rarity;	// Use ship rarity instead of schematic item rarity
@@ -61,31 +58,70 @@ class UnneededItems extends Component<UnneededItemsProps, UnneededItemsState> {
 			})
 		);
 
-		this.setState({ fuellist, fuelschematicslist });
+		// Calculate all replicator fodder
+		// 	Only consider equipment of already immortalized crew as fodder
+		//	This may miss out on items for crew who are mid-level (or even fully equipped Lvl 99s)
+		let equipmentAlreadyOnCrew = new Set();
+		let equipmentNeededByCrew = new Set();
+		playerData.player.character.crew.forEach(crew => {
+			crew.equipment_slots.forEach(equipment => {
+				if (crew.immortal > 0)
+					equipmentAlreadyOnCrew.add(equipment.symbol);
+				else
+					equipmentNeededByCrew.add(equipment.symbol);
+			});
+		});
+		let fuellist = items.filter(
+			item => equipmentAlreadyOnCrew.has(item.symbol) &&
+					!equipmentNeededByCrew.has(item.symbol)
+		);
+
+		// Should probably regex this
+		//	Fancy apostrophe check needed for some crew
+		const isSpecificItem = (name) =>
+			name.indexOf("'s ") > 0 ||
+			name.indexOf("s' ") > 0 ||
+			name.indexOf("’s ") > 0 ||
+			name.indexOf("s’ ") > 0;
+
+		// Filter crew-specific items
+		let fuelspecific = fuellist.filter(
+			item => isSpecificItem(item.name)
+		);
+
+		// Filter generic items
+		let fuelgeneric = fuellist.filter(
+			item =>
+				item.quantity === 1 && item.rarity > 1 &&
+				!isSpecificItem(item.name)
+		);
+
+		this.setState({ fuelschematics, fuelspecific, fuelgeneric });
 	}
 
 	render() {
 		const { playerData } = this.props;
 
 		let itemCount = playerData.player.character.items.length;
-		let schematicsCount = this.state.fuelschematicslist.length;
-		let fuelCount = this.state.fuellist.length;
+		let itemLimit = 1000, itemWarning = .9*itemLimit;
+		// Hardcoded limit works now, but if the game increases limit, we'll have to update
+		//	We should get this from playerData.player.character.item_limit, but it's not in preparedProfileData
 
 		return (
 			<div>
-				{itemCount > 900 && (
+				{itemCount > itemWarning && (
 					<Message warning>
 						<Message.Header>Items approaching limit</Message.Header>
 						<p>
-							You have {itemCount} items in your inventory. At {playerData.player.character.item_limit} the game starts randomly losing
+							You have {itemCount} items in your inventory. At {itemLimit} the game starts randomly losing
 							items; go and replicate away some unnecessary stuff.
 						</p>
 					</Message>
 				)}
 
-				<Header as='h4'>Here are {schematicsCount} potential items that you don't need (used to upgrade ships you already maxed):</Header>
+				<Header as='h4'>Here are {this.state.fuelschematics.length} items that you don't need (used to upgrade ships you already maxed):</Header>
 				<Grid columns={5} centered padded>
-					{this.state.fuelschematicslist.map((item, idx) => (
+					{this.state.fuelschematics.map((item, idx) => (
 						<Grid.Column key={idx} rel={item.archetype_id} textAlign='center'>
 							<ItemDisplay
 								src={`${process.env.GATSBY_ASSETS_URL}${item.imageUrl}`}
@@ -98,9 +134,24 @@ class UnneededItems extends Component<UnneededItemsProps, UnneededItemsState> {
 					))}
 				</Grid>
 
-				<Header as='h4'>Here are {fuelCount} potential items that you don't need (used to equip crew you already equipped):</Header>
+				<Header as='h4'>Here are {this.state.fuelspecific.length} items that you don't need (used to equip specific crew you already equipped):</Header>
 				<Grid columns={5} centered padded>
-					{this.state.fuellist.map((item, idx) => (
+					{this.state.fuelspecific.map((item, idx) => (
+						<Grid.Column key={idx} rel={item.archetype_id} textAlign='center'>
+							<ItemDisplay
+								src={`${process.env.GATSBY_ASSETS_URL}${item.imageUrl}`}
+								size={64}
+								maxRarity={item.rarity}
+								rarity={item.rarity}
+							/>
+							<p>{item.name}</p>
+						</Grid.Column>
+					))}
+				</Grid>
+
+				<Header as='h4'>Here are {this.state.fuelgeneric.length} items that you don't need now, but might be useful in the future:</Header>
+				<Grid columns={5} centered padded>
+					{this.state.fuelgeneric.map((item, idx) => (
 						<Grid.Column key={idx} rel={item.archetype_id} textAlign='center'>
 							<ItemDisplay
 								src={`${process.env.GATSBY_ASSETS_URL}${item.imageUrl}`}
