ed06f652ffda06919c95451239e32bd119073ef9
TemporalAgent7 <admin@datacore.app>
2021-03-07 00:54:37 -0800

    (WIP) Switch to Gatsby3; fix theming

--- static/styles/theming.js
+++ static/styles/theming.js
@@ -1,31 +1,28 @@
-const g_defaultTheme = '/styles/semantic.css';
+const g_liteTheme = '/styles/semantic.css';
 const g_darkTheme = '/styles/semantic.slate.css';
 
-function swapThemeCss() {
-	let sheet = document.querySelector('#themeCSS');
-	if (!sheet) {
-		return;
-	}
-
-	if (sheet.getAttribute('href') === g_defaultTheme) {
-		sheet.setAttribute('href', g_darkTheme);
-		window.localStorage.setItem('theme', 'dark');
-	} else {
-		sheet.setAttribute('href', g_defaultTheme);
-		window.localStorage.setItem('theme', 'lite');
-	}
-}
-
 function setThemeCss(dark) {
-	let sheet = document.querySelector('#themeCSS');
+	let sheet = document.getElementById('themeCSS');
 	if (!sheet) {
 		return;
 	}
 
-	sheet.setAttribute('href', dark ? g_darkTheme : g_defaultTheme);
+	window.localStorage.setItem('theme', dark ? 'dark' : 'lite');
+	sheet.setAttribute('href', dark ? g_darkTheme : g_liteTheme);
 }
 
-let theme = (windowGlobal && window.localStorage && window.localStorage.getItem('theme')) ? window.localStorage.getItem('theme') : 'lite';
-if (theme) {
-	setThemeCss(theme === 'dark');
+function swapThemeCss() {
+	let sheet = document.getElementById('themeCSS');
+	if (!sheet) {
+		return;
+	}
+
+	setThemeCss(sheet.getAttribute('href') === g_liteTheme);
+}
+
+if (window) {
+	window.onload = function() {
+		let theme = (window && window.localStorage) ? window.localStorage.getItem('theme') : 'dark';
+		setThemeCss(theme === 'dark');
+	};
 }
