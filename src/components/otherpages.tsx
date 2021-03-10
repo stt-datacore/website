43cebfe3a7512b85e0f08e8bc9397275ff61c885
TemporalAgent7 <alejandro93@protonmail.com>
2021-03-10 11:14:34 -0800

    Add way to exclude pages from Other. Exclude About page since it's already a top-level item.

--- src/components/otherpages.tsx
+++ src/components/otherpages.tsx
@@ -6,20 +6,27 @@ export type PageEntry = {
 };
 
 export const useOtherPages = (): PageEntry[] => {
-	const { allMarkdownRemark } = useStaticQuery(graphql`query {
-		allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(/static/pages)/.*\\.md$/"}, frontmatter: {bigbook_section: {eq: null}}}) {
-			edges {
-			  node {
-				frontmatter {
-				  title
-				  bigbook_section
-				}
-				fields {
-				  slug
-				}
-			  }
+	const { allMarkdownRemark } = useStaticQuery(graphql`query{
+		allMarkdownRemark(
+			filter: {
+				fileAbsolutePath: { regex: "/(/static/pages)/.*\\.md$/" }
+				frontmatter: { bigbook_section: { eq: null }, hide_in_other: { ne: true } }
 			}
-		  }}`);
+		) {
+			edges {
+				node {
+					frontmatter {
+						title
+						bigbook_section
+						hide_in_other
+					}
+					fields {
+						slug
+					}
+				}
+			}
+		}
+	}`);
 
 	return allMarkdownRemark.edges.map(({ node }) => (
 		{ slug: node.fields.slug, title: node.frontmatter.title }
