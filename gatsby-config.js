6fc79f376b972c5399e39e5531c1f8d6324bef2c
Effy Elden <git@effy.is>
2021-04-11 17:41:23 +1000

    Test fixing deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
 			options: {
 				token: process.env.CLOUDFLARE_TOKEN,
 				zoneId: process.env.CLOUDFLARE_ZONE_ID,
-				condition: (api, options) => process.env.GITHUB_REF === 'refs/heads/master',
+				condition: (api, options) => process.env.GITHUB_REF === 'refs/heads/masterTEST',
 			}
 		},
 		{
