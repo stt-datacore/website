2013ba2ce1581f225172752abfbc66a46107e8f9
Effy Elden <git@effy.is>
2021-04-20 22:40:44 +1000

    Add linebreak to stop ugly wrapping

--- src/components/profile_crew.tsx
+++ src/components/profile_crew.tsx
@@ -100,6 +100,7 @@ const ProfileCrew = (props: ProfileCrewProps) => {
 					{crew.favorite && <Icon name="heart" />}
 					<span>Level {crew.level}, </span>
 					<span>Tier {formatTierLabel(crew.bigbook_tier)}, </span>
+					<br />
 					<span>{crew.events} events, </span>
 					<span>{crew.collections.length} collections</span>
 				</div>
