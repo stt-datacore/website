fdd76959a8b2ec9caa5143a848852a96ca29bfaf
ussjohnjay <76980548+ussjohnjay@users.noreply.github.com>
2021-10-20 14:15:22 -0500

    Announcements page with dismissal and excerpts
    More fully-featured version of the announcements function introduced last week.
    
    This sets up an announcements page that lists the 20 most recent announcements at datacore.app/announcements. Announcements can now link excerpts on the index page to the announcements page for more details. An amended version of the new voyage calc announcement demos this.
    
    Announcements on the index page can now be dismissed. Otherwise announcements will expire 7 days after the post date. Resolves #299.
    
    I have yet to figure out an easy way to handle #300. Any suggestions would be welcome!
    
    This separates the announcements functions to their own page and component. Also fixes the unintentional "feature" of creating pages for each individual announcement.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,17 @@ module.exports = {
 		siteUrl: 'https://datacore.app'
 	},
 	plugins: [
-		`gatsby-transformer-remark`,
 		`gatsby-transformer-json`,
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-remove-fingerprints`,
 		`gatsby-plugin-sitemap`,
 		`gatsby-plugin-typescript`,
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				excerpt_separator: `<!-- end -->`,
+			}
+		},
 		{
 			resolve: 'gatsby-plugin-purge-cloudflare-cache',
 			options: {
