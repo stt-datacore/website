fee9d879c65dd504ee1ee8d63b3b8e9120f0152c
ussjohnjay <jtjasmin+stt@gmail.com>
2023-01-22 14:39:26 -0600

    Break ties by crew name
    Potential fix for crew rank finder bug, by breaking voyage skill score ties by crew name.

--- src/components/voyagecalculator/lineupviewer.tsx
+++ src/components/voyagecalculator/lineupviewer.tsx
@@ -29,9 +29,15 @@ const LineupViewer = (props: LineupViewerProps) => {
 			const skill = skillRankings.find(sr => sr.skill === crewSkill);
 			const rank = skill.roster.filter(c =>
 				Object.keys(c.skills).includes(seatSkill)
-					&& voyScore(c.skills[crewSkill]) > voyScore(crew.skills[crewSkill])
+					&& voyScore(c.skills[crewSkill]) >= voyScore(crew.skills[crewSkill])
 					&& !usedCrew.includes(c.id)
-			).length + 1;
+			).sort((c1, c2) => {
+				// Break ties by crew name
+				const s1 = voyScore(c1.skills[crewSkill]);
+				const s2 = voyScore(c2.skills[crewSkill]);
+				if (s1 === s2) return c1.name.localeCompare(c2.name);
+				return s2 - s1;
+			}).map(c => c.id).indexOf(crew.id) + 1;
 			// Prefer seat skill if no scrolling is necessary
 			const stayWithSeat = best.skill === seatSkill && best.rank <= 3;
 			const switchToSeat = crewSkill === seatSkill && (rank <= 3 || rank === best.rank);
