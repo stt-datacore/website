4f8d72e837f2b72b282e643e4fc454def12f4349
Nathaniel Moschkin <precise.tester@gmail.com>
2023-06-08 00:38:08 -0400

    Fix worker loader type

--- src/typings/worker.d.ts
+++ src/typings/worker.d.ts
@@ -1,4 +1,10 @@
 declare module "worker-loader!*" {
-  const content: any;
-  export = content;
+  // You need to change `Worker`, if you specified a different value for the `workerType` option
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+
+  // Uncomment this if you set the `esModule` option to `false`
+  // export = WebpackWorker;
+  export = WebpackWorker;
 }
