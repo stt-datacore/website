79ba026b038a20a63e2b49e03c6c5ae1efe21609
Nathaniel Moschkin <precise.tester@gmail.com>
2023-06-24 23:02:06 -0400

    Add merged ships to round out global player context

--- src/context/playercontext.tsx
+++ src/context/playercontext.tsx
@@ -4,12 +4,15 @@ import { useStateWithStorage } from '../utils/storage';
 import { DataContext, DataProviderProperties } from './datacontext';
 import { BuffStatTable, calculateBuffConfig } from '../utils/voyageutils';
 import { prepareProfileData } from '../utils/crewutils';
+import { Ship } from '../model/ship';
+import { mergeShips } from '../utils/shiputils';
 
 export interface PlayerContextData {
 	strippedPlayerData?: PlayerData;
 	setStrippedPlayerData: (playerData: PlayerData | undefined) => void; 
 	buffConfig?: BuffStatTable;
 	preparedPlayerData?: PlayerData;
+	mergedShips?: Ship[];
 }
 
 const defaultPlayer = {
@@ -25,18 +28,24 @@ export const PlayerProvider = (props: DataProviderProperties) => {
 	
 	const buffConfig = strippedPlayerData ? calculateBuffConfig(strippedPlayerData.player) : undefined;
 	let prep: PlayerData | undefined = undefined;
+	let ships: Ship[] | undefined = undefined;
 
 	// PlayerContext will never demand the data. It will only use it if it's there.
 	if (strippedPlayerData && dataContext && dataContext.crew?.length) {
 		prep = JSON.parse(JSON.stringify(strippedPlayerData)) as PlayerData;
 		prepareProfileData('PROFILE_CONTEXT', dataContext.crew, prep, new Date());
+
+		if (dataContext.ship_schematics) {
+			ships = mergeShips(dataContext.ship_schematics, prep.player.character.ships);				
+		}
 	}
 
 	const context = {
 		strippedPlayerData,
 		setStrippedPlayerData,
 		buffConfig,
-		preparedPlayerData: prep
+		preparedPlayerData: prep,
+		mergedShips: ships
 	} as PlayerContextData;
 
 	return (
