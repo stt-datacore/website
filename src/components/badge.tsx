f519473e4ef5dcff483cde7c5ea72b45e4aa02b9
Nathaniel Moschkin <precise.tester@gmail.com>
2023-07-03 22:04:20 -0400

    Slowly working on badge

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -5,6 +5,8 @@ import React from "react";
 
 export interface BadgeProps {
     value?: number;
+    color?: string;
+    border?: string;
     children?: JSX.Element;
 }
 
@@ -16,13 +18,42 @@ export class Badge extends React.Component<BadgeProps> {
     }
 
 
+    componentDidMount(): void {
+        
+    }
 
     render() {
 
-        const { children } = this.props;
+        const { value, color, border, children } = this.props;
+        let usecolor: string;
 
+        if (!color) {
+            usecolor = "aquamarine"
+        }
+        else {
+            usecolor = color;
+        }
         return (
-        <div>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+            height: "1.5em"
+        }}>
+            <div className="ui" style={{
+                textAlign: "center",
+                position: "relative",
+                backgroundColor: usecolor,       
+                color: "darkgray",         
+                border,
+                top: "-0.5em",
+                right: "-3em",
+                width: "1.2em",
+                height: "1.2em",
+                borderRadius: "0.6em"
+            }}>
+                {value}
+            </div>
             {children}    
         </div>)
     }
