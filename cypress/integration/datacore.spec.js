4c96a1a03c029a817656eb9b376a0cf57ed7d5a9
Nathaniel Moschkin <precise.tester@gmail.com>
2023-09-02 21:44:41 -0400

    Fix up Cypress tests

--- cypress/integration/datacore.spec.js
+++ cypress/integration/datacore.spec.js
@@ -17,7 +17,7 @@ describe('Crew page', () => {
     cy.visit('/crew/dsc_adira_tal_new_crew/');
 
     // Check flavour text
-    cy.get('p').contains('their memories');
+    cy.get('#crew_flavor_text').contains('their memories');
     // Check ranks are showing
     cy.get('.label').contains('Voyage Rank');
   })
