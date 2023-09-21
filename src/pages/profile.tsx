e1d86c08ec54cb4593cc14fbb28202d918dc40ef
Nathaniel Moschkin <precise.tester@gmail.com>
2023-09-20 23:57:40 -0400

    Null check

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -194,7 +194,7 @@ class ProfilePageComponent extends Component<ProfilePageComponentProps, ProfileP
 			}
 		];
 
-		const avatar = `${process.env.GATSBY_ASSETS_URL}${playerData.player.character.crew_avatar
+		const avatar = `${process.env.GATSBY_ASSETS_URL}${playerData?.player.character.crew_avatar
 			? (playerData.player.character.crew_avatar.portrait.file ?? playerData.player.character.crew_avatar.portrait)
 			: 'crew_portraits_cm_empty_sm.png'
 		}`;
