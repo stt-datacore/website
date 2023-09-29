43f99f14f8db44fa4651ed260b3cfa58ed41012d
Nathaniel Moschkin <precise.tester@gmail.com>
2023-09-29 17:22:28 -0400

    Fix double-buffing player levels

--- src/components/item_presenters/classic_presenter.tsx
+++ src/components/item_presenters/classic_presenter.tsx
@@ -333,7 +333,7 @@ export const Skills = (props: SkillsProps) => {
 		if (!debasedCrew.base_skills[skill])
 			delete debasedCrew.base_skills[skill];
 	});
-	const buffedSkills = buffConfig ? applyCrewBuffs(debasedCrew, buffConfig) : undefined;
+	const buffedSkills = (!!buffConfig && !playerLevels) ? applyCrewBuffs(debasedCrew, buffConfig) : undefined;
 
 	const baseSkills = Object.entries(buffedSkills ?? skills)
 		.filter(base_skills => !!base_skills[1])
