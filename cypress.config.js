6ac75436bb78b47f7940c997ac7b411c292ba2d9
Nathaniel Moschkin <precise.tester@gmail.com>
2024-04-23 01:33:16 -0400

    add video to cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   defaultCommandTimeout: 20000,
   requestTimeout: 20000,
+  video: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
