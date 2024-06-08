2fb9f15668db4151725bd5e4055b7acdd7d9e884
Nathaniel Moschkin <precise.tester@gmail.com>
2024-06-08 01:01:52 -0400

    Merge schematics with wiki data.

--- src/context/datacontext.tsx
+++ src/context/datacontext.tsx
@@ -15,6 +15,7 @@ import { getItemWithBonus } from '../utils/itemutils';
 import { EventInstance } from '../model/events';
 import { StaticFaction } from '../model/shuttle';
 import { getSkillOrder, getVoyageQuotient } from '../utils/crewutils';
+import { highestLevel } from '../utils/shiputils';
 
 const DC_DEBUGGING: boolean = false;
 
@@ -519,7 +520,16 @@ export const DataProvider = (props: DataProviderProperties) => {
 				}
 			}
 
-			let scsave = data.ship_schematics.map((sc => JSON.parse(JSON.stringify({ ...sc.ship, level: sc.ship.level + 1 })) as Ship));
+			let scsave = data.ship_schematics.map((sc => JSON.parse(JSON.stringify({ ...sc.ship, level: 0 })) as Ship));
+			for (let ship of scsave) {
+				if (ship.levels) {
+					let n = highestLevel(ship);
+					if (n) {
+						ship = { ...ship, ...ship.levels[`${n}`] };
+						ship.level = n -1;						
+					}
+				}	
+			}
 			data.ships = scsave;
 		}
 	}
