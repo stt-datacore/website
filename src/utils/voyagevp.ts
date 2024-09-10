561a0a75480c263da50bc0ab809497010896e739
Nathaniel Moschkin <ironywrit@gmail.com>
2024-09-10 16:04:16 -0400

    Multiply encounter by opponents.

--- src/utils/voyagevp.ts
+++ src/utils/voyagevp.ts
@@ -52,7 +52,7 @@ export function calcVoyageVP(seconds: number, bonuses: number[]): VPDetails {
 
     drops.forEach((drop) => {
         if (seconds >= drop.end) {
-            total += (dropvp * drop.drops * drop.actual) + drop.encounter;
+            total += (dropvp * drop.drops * drop.actual) + (drop.encounter * drop.opponents);
             vpdetails.total_drops += drop.drops;
             vpdetails.total_encounters++;
             vpdetails.total_opponents += drop.opponents;
