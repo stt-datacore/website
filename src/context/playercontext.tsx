550a8f358161c605272faed06acb44ec1dc0cc7c
Nathaniel Moschkin <precise.tester@gmail.com>
2024-09-14 21:26:45 -0400

    Consider crew with same short name that are missing variant trait Add sort-by release date feature.

--- src/context/playercontext.tsx
+++ src/context/playercontext.tsx
@@ -156,7 +156,6 @@ export const PlayerProvider = (props: DataProviderProperties) => {
 
 		const quipment = coreData.items.filter(i => i.type === 14).map(i => getItemWithBonus(i));
 		prepareProfileData('PLAYER_CONTEXT', coreData.crew, preparedProfileData, dtImported, quipment);
-
 		setProfile(preparedProfileData);
 
 		if (preparedProfileData) {
