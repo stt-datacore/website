8781ce9955f0d5ffa395aad0d6f808f30e9bbea5
Nathaniel Moschkin <precise.tester@gmail.com>
2024-09-18 18:23:51 -0400

    add attributes for encounter voyages.

--- src/model/player.ts
+++ src/model/player.ts
@@ -1100,8 +1100,17 @@ export interface Voyage {
   seconds_since_last_dilemma: number
   first_leave: boolean
   time_to_next_event: number
-  ship_id: number
+  ship_id: number;
+  next_interaction: number;
   crew_slots: VoyageCrewSlot[]
+}
+
+export interface EncounterVoyage extends Voyage {
+    phase_id: number;
+    event_instance_id: number;
+    encounter: number;
+    encounter_skip_boost?: number;
+    fast_forward_boost?: number;
 }
 
 export interface PendingRewards {
