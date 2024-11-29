07c2eaaa8a24a510c625be5cd325248068c73f11
Nathaniel Moschkin <precise.tester@gmail.com>
2024-11-29 15:26:37 -0500

    Add total power difference.

--- src/components/stats/statsmain.tsx
+++ src/components/stats/statsmain.tsx
@@ -9,6 +9,7 @@ import { StatTrendsTable } from "./table";
 import { StatsPrefsPanel } from "./prefspanel";
 import { DEFAULT_MOBILE_WIDTH } from "../hovering/hoverstat";
 import { StatsContext } from "./dataprovider";
+import { Skill } from "../../model/crew";
 
 export const StatTrendsComponent = () => {
     const globalContext = React.useContext(GlobalContext);
@@ -16,10 +17,12 @@ export const StatTrendsComponent = () => {
     const crew = [...globalContext.core.crew].sort((a, b) => a.date_added.getTime() - b.date_added.getTime());
     const { t, tfmt } = globalContext.localized;
 
+    const [totalPowerDiff, setTotalPowerDiff] = React.useState([] as Skill[]);
     const [avgVelocity, setAvgVelocity] = React.useState(0);
     const [meanVelocity, setMeanVelocity] = React.useState(0);
     const [avgDaysBetween, setAvgDaysBetween] = React.useState(0);
     const [meanDaysBetween, setMeanDaysBetween] = React.useState(0);
+
     const { epochDiffs, skillKey, displayMode, setDisplayMode, crewCount } = statsContext;
 
     React.useEffect(() => {
@@ -47,9 +50,26 @@ export const StatTrendsComponent = () => {
             setAvgDaysBetween(0);
             setMeanDaysBetween(0);
         }
+
+        if (skillKey) {
+            const pd = epochDiffs.map(ed => ed.aggregates.map(m => m.slice(0, ed.skills.length)));
+            const skilldiffs = [] as Skill[];
+            epochDiffs[0].skills.map((skill, idx) => {
+                let first = epochDiffs[epochDiffs.length - 1].aggregates[1][idx];
+                let last = epochDiffs[0].aggregates[0][idx];
+                let diff = last - first;
+                skilldiffs.push({
+                    core: diff,
+                    skill,
+                    range_max: 0,
+                    range_min: 0
+                })
+            });
+            setTotalPowerDiff(skilldiffs);
+        }
     }, [epochDiffs]);
 
-    const gridWidth = 5;
+    const gridWidth = 8;
     const isMobile = typeof window !== 'undefined' && window.innerWidth < DEFAULT_MOBILE_WIDTH;
 
     const statsStyle: React.CSSProperties = { width: '100%', height: '3em', margin: 0 };
@@ -112,11 +132,38 @@ export const StatTrendsComponent = () => {
     function renderStatsInfo() {
         return (
             <div className="ui segment">
-                <Label style={{ textAlign: 'center' }}>
-                    <h2>{skillDecors}</h2>
-                    <div className='ui segment' style={{ backgroundColor: 'navy' }}>{crewCount} {t('base.crewmen')}</div>
-                </Label>
                 <Grid style={{ margin: '1em -1em', gap: '0' }}>
+                    <Grid.Row style={{ padding: '0.5em' }}>
+                        <Grid.Column width={gridWidth} style={{ padding: '0 0.5em' }}>
+                            <Label style={{ textAlign: 'center', width: "100%" }}>
+                                <h2>{skillDecors}</h2>
+                                <div className='ui segment' style={{ backgroundColor: 'navy' }}>
+                                    {crewCount} {t('base.crewmen')}
+                                </div>
+                            </Label>
+                        </Grid.Column>
+                        <Grid.Column width={gridWidth} style={{ padding: '0 0.5em' }}>
+                            {!!skillKey && <Label style={{ textAlign: 'center', width: "100%" }}>
+                                <h2>{t('stat_trends.total_power_difference')}</h2>
+                                <div className='ui segment' style={{
+                                    backgroundColor: 'navy',
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    gap: '1em'
+                                    }}>
+                                    {totalPowerDiff.map((skill) => {
+
+                                        return <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '0.5em'}}>
+                                            <img src={skillIcon(skill.skill)} style={{height: '1.2em'}} />
+                                            {skill.core > 0 ? "+" : ""}{skill.core.toFixed(2)}
+                                        </div>
+                                    })}
+                                </div>
+                            </Label>}
+                        </Grid.Column>
+                    </Grid.Row>
                     <Grid.Row style={{ padding: '0.5em' }}>
                         <Grid.Column width={gridWidth} style={{ padding: '0 0.5em' }}>
                             <StatLabel style={statsStyle} title={t('stat_trends.stats.average_velocity')} value={avgVelocity?.toFixed(2)} />
