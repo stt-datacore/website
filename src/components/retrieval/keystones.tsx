aa064bbea13c637e4a8a6263b03626c25d51d744
Nathaniel Moschkin <precise.tester@gmail.com>
2024-12-16 14:12:04 -0500

    revert correcting DBID key for retrieval options.

--- src/components/retrieval/keystones.tsx
+++ src/components/retrieval/keystones.tsx
@@ -109,7 +109,8 @@ const ModePicker = (props: ModePickerProps) => {
 	const { playerData } = globalContext.player;
 	const { t } = globalContext.localized;
 	const { allKeystones, market, reloadMarket } = props;
-	const dbid = props.dbid ? props.dbid + '/' : '';
+	//const dbid = props.dbid ? props.dbid?.toString() + '/' : '';
+	const dbid = props.dbid ? props.dbid?.toString() : '';
 	const [mode, setMode] = useStateWithStorage<ModePickerMode>(`${dbid}keystone_modePicker`, 'keystones');
 	const isMobile = typeof window !== 'undefined' && window.innerWidth < DEFAULT_MOBILE_WIDTH;
 
@@ -170,19 +171,9 @@ const KeystonesPlayer = (props: KeystonesPlayerProps) => {
 	const [polestarTailors, setPolestarTailors] = useStateWithStorage<IPolestarTailors>(dbid+'retrieval/tailors', polestarTailorDefaults, { rememberForever: true });
 	const [crewFilters, setCrewFilters] = useStateWithStorage<ICrewFilters>(dbid+'retrieval/filters', crewFilterDefaults, { rememberForever: true });
 
-
-	const [legacyWishlist, setLegacyWishlist] = useStateWithStorage<string[]>(dbid.replace('/', '')+'retrieval/wishlist', [], { rememberForever: true });
-
 	const [wishlist, setWishlist] = useStateWithStorage<string[]>(dbid+'retrieval/wishlist', [], { rememberForever: true });
 	const [autoWish, setAutoWish] = useStateWithStorage<boolean>(dbid+'retrieval/auto_wishlist', false, { rememberForever: true });
 	const [autoWishes, setAutoWishes] = React.useState<string[]>([]);
-
-	React.useEffect(() => {
-		if (legacyWishlist?.length) {
-			setWishlist(legacyWishlist);
-			setLegacyWishlist([]);
-		}
-	}, [legacyWishlist]);
 
 	React.useEffect(() => {
 		const allKeystones = JSON.parse(JSON.stringify(props.allKeystones)) as IKeystone[];
