5e8a15d64aa78093a862ae10bcf1e74e172d0a97
Nathaniel Moschkin <precise.tester@gmail.com>
2024-12-20 21:41:39 -0500

    Break up voyage stats

--- src/components/voyagecalculator/voyagestats.tsx
+++ src/components/voyagecalculator/voyagestats.tsx
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import { Table, Grid, Header, Accordion, Segment, Message, Dimmer, Loader } from 'semantic-ui-react';
-import { isMobile } from 'react-device-detect';
+import { Accordion, Segment, Message, Dimmer, Loader } from 'semantic-ui-react';
 
 import { LineupViewer } from './lineupviewer';
-import ItemDisplay from '../itemdisplay';
 
 import { UnifiedWorker as Worker } from '../../typings/worker';
-import { ResponsiveLineCanvas } from '@nivo/line';
-import themes from '../nivo_themes';
-import { PlayerCrew, PlayerData, PlayerEquipmentItem, Reward, Voyage } from '../../model/player';
+import { PlayerCrew, PlayerData, PlayerEquipmentItem, Voyage } from '../../model/player';
 import { Ship } from '../../model/ship';
 import { Estimate, ExtendedVoyageStatsConfig, VoyageStatsConfig } from '../../model/worker';
 import { CrewMember } from '../../model/crew';
 import { EquipmentCommon } from '../../model/equipment';
-import { checkReward, mergeItems } from '../../utils/itemutils';
 import { GlobalContext } from '../../context/globalcontext';
 import { formatTime } from '../../utils/voyageutils';
-import { DEFAULT_MOBILE_WIDTH } from '../hovering/hoverstat';
-import { IEventData } from '../eventplanner/model';
 import { QuipmentProspectList } from './quipmentprospects';
-import { ItemHoverStat } from '../hovering/itemhoverstat';
+import { VoyageStatsEstimate, VoyageStatsEstimateTitle } from './stats/statsestimate';
+import { VoyageStatsRewardsTitle, VoyageStatsRewards } from './stats/statsrewards';
 
 type VoyageStatsProps = {
 	configSource?: 'player' | 'custom';
@@ -173,101 +167,6 @@ export class VoyageStats extends Component<VoyageStatsProps, VoyageStatsState> {
 		// return hours+"h " +minutes+"m";
 	}
 
-	_renderChart(needsRevive?: boolean) {
-		const estimate = this.props.estimate ?? this.state.estimate;
-		const { t } = this.context.localized;
-		const names = needsRevive ? [t('voyage.estimate.first_refill'), t('voyage.estimate.second_refill')]
-															: [ t('voyage.estimate.no_refills'), t('voyage.estimate.one_refill'), t('voyage.estimate.two_refills')];
-		const isMobile = typeof window !== 'undefined' && window.innerWidth < DEFAULT_MOBILE_WIDTH;
-		const rawData = needsRevive ? estimate?.refills : estimate?.refills.slice(0, 2);
-		// Convert bins to percentages
-		const data = estimate?.refills.map((refill, index) => {
-			var bins = {} as Bins;
-			const binSize = 1/30;
-
-			for (var result of refill.all.sort()) {
-				const bin = Math.floor(result/binSize)*binSize+binSize/2;
-
-			  try{
-				++bins[bin].count;
-			  }
-			  catch {
-				bins[bin] = {result: bin, count: 1};
-			  }
-			}
-
-			delete bins[NaN];
-			var refillBins = Object.values(bins);
-
-			const total = refillBins
-				.map(value => value.count)
-				.reduce((acc, value) => acc + value, 0);
-			var aggregate = total;
-			const cumValues = value => {
-				aggregate -= value.count;
-				return {x: value.result, y: (aggregate/total)*100};
-			};
-			const ongoing = value => { return {x: value.result, y: value.count/total}};
-
-			const percentages = refillBins
-				.sort((bin1, bin2) => bin1.result - bin2.result)
-				.map(cumValues);
-
-			return {
-				id: names[index],
-				data: percentages
-			};
-		});
-		if (!data) return <></>;
-
-		return (
-			<div style={{height : 200}}>
-				<ResponsiveLineCanvas
-					data={data}
-					xScale= {{type: 'linear', min: data[0].data[0].x}}
-					yScale={{type: 'linear', max: 100 }}
-					theme={themes.dark}
-					axisBottom={{legend : t('voyage.estimate.legend.voyage_length'), legendOffset: 30, legendPosition: 'middle'}}
-					axisLeft={{legend : t('voyage.estimate.legend.chance_%'), legendOffset: -36, legendPosition: 'middle'}}
-					margin={{ top: 50, right: 170, bottom: 50, left: 60 }}
-					enablePoints= {true}
-					pointSize={0}
-					crosshairType={undefined}
-					tooltip={input => {
-						let data = input.point.data;
-						return <>{input.point.serieId}:
-						{t('voyage.estimate.n_chance_of_reaching_t', {
-							n: (data.y as number).toFixed(2),
-							t: this._formatTime(data.x as number)
-						})}
-						</>;
-					}}
-					legends={[
-						{
-							anchor: 'bottom-right',
-							direction: 'column',
-							justify: false,
-							translateX: 120,
-							translateY: 0,
-							itemsSpacing: 2,
-							itemWidth: 100,
-							itemHeight: 20,
-							symbolSize: 20,
-							effects: [
-								{
-									on: 'hover',
-									style: {
-										itemOpacity: 1,
-									},
-								},
-							],
-						},
-					]}
-				/>
-			</div>
-		);
-	}
-
 	_renderQuipmentProspects() {
 		const { configSource, voyageData, roster, rosterType } = this.props;
 		const crew = voyageData.crew_slots.map(s => s.crew);
@@ -278,310 +177,6 @@ export class VoyageStats extends Component<VoyageStatsProps, VoyageStatsState> {
 		const { configSource, voyageData, roster, rosterType } = this.props;
 		if (!this.ship || !roster) return <></>;
 		return <LineupViewer configSource={configSource} voyageConfig={voyageData} ship={this.ship} roster={roster} rosterType={rosterType} />;
-	}
-
-	_renderEstimateTitle(needsRevive: boolean = false) {
-		const estimate  = this.props.estimate ?? this.state.estimate;
-		const { t } = this.context.localized;
-		return needsRevive || !estimate
-			?	t('voyage.estimate.estimate')
-			: t('voyage.estimate.estimate_time', {
-				time: this._formatTime(estimate['refills'][0].result)
-			});
-	}
-
-	_renderPrettyCost(cost: number, idx: number) {
-		if (this.context.player.playerData && cost) {
-			const { t } = this.context.localized;
-			let revivals = this.context.player.playerData.player.character.items.find(f => f.symbol === 'voyage_revival');
-
-			if (revivals && revivals.quantity && revivals.quantity >= idx) {
-				let globalItem = this.context.core.items.find(f => f.symbol === 'voyage_revival');
-				if (globalItem) {
-					revivals = mergeItems([revivals], [globalItem])[0];
-					return <div style={{
-						display: "flex",
-						flexDirection: "row",
-						alignItems: "center",
-						justifyContent: "left"
-					}}>
-						<div style={{width: "32px"}}>
-						<img style={{height:"24px", margin:"0.5em"}} src={`${process.env.GATSBY_ASSETS_URL}${revivals.imageUrl}`} />
-						</div>
-						<span>{idx} / {revivals.quantity} {t('global.item_types.voyage_consumable')}</span>
-					</div>
-				}
-			}
-			else {
-				return <div style={{
-					display: "flex",
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "left"
-				}}>
-					<div style={{width: "32px"}}>
-					<img style={{height:"24px", margin:"0.5em"}} src={`${process.env.GATSBY_ASSETS_URL}atlas/pp_currency_icon.png`} />
-					</div>
-					<span>{cost} {t('global.item_types.dilithium')}</span>
-				</div>
-			}
-		}
-
-
-		return <>{cost == 0 || 'Costing ' + cost + ' dilithium'}</>
-	}
-
-	_renderEstimate(needsRevive: boolean = false) {
-		const estimate  = this.props.estimate ?? this.state.estimate;
-		const { t, tfmt } = this.context.localized;
-		if (!estimate)
-			return (<div>{t('spinners.calculating_voyage_estimate')}</div>);
-
-		const renderEst = (label, refills, idx) => {
-			if (refills >= estimate['refills'].length) return (<></>);
-			const est = estimate['refills'][refills];
-			return (
-				<tr>
-					<td>{label}: {this._formatTime(est.result)}</td>
-					{!isMobile && <td>90%: {this._formatTime(est.safeResult)}</td>}
-					<td>99%: {this._formatTime(est.saferResult)}</td>
-					<td>
-						{t('voyage.estimate.dilemma_chance', {
-							time: `${est.lastDil}`,
-							chance: `${Math.floor(est.dilChance)}`
-						})}
-					</td>
-					<td>{this._renderPrettyCost(est.refillCostResult, idx)}</td>
-				</tr>
-			);
-		};
-
-		if (estimate.deterministic) {
-			let extendTime = estimate['refills'][1].result - estimate['refills'][0].result;
-			let refill = 0;
-
-			return (
-				<div>
-					{t('voyage.estimate.voyage_end', {
-						time: this._formatTime(estimate['refills'][0].result)
-					})}
-					{t('voyage.estimate.voyage_extend', {
-						time: this._formatTime(extendTime)
-					})}
-					{/*
-					For a {this.config?.selectedTime ?? 20} hour voyage you need {estimate['refillshr20']} refills at a cost of {estimate['dilhr20']} dilithium (or {estimate['refillshr20']} voyage revivals.) */}
-										<Table style={{marginTop:"0.5em"}}><tbody>
-						{!needsRevive && renderEst(t('voyage.estimate.estimate'), refill++, 0)}
-						{renderEst(t('voyage.estimate.1_refill'), refill++, 1)}
-						{renderEst(t('voyage.estimate.n_refills', { n: '2' }), refill++, 2)}
-					</tbody></Table>
-					<p>
-						{t('voyage.estimate.voyage_long_advice', {
-							h: `${this.config?.selectedTime ?? 20}`,
-							r: `${estimate['refillshr20']}`,
-							d: `${estimate['dilhr20']}`,
-							re: `${estimate['refillshr20']}`
-						})}
-					</p>
-				</div>
-			);
-		} else {
-			let refill = 0;
-
-			return (
-				<div>
-					<Table><tbody>
-						{!needsRevive && renderEst(t('voyage.estimate.estimate'), refill++, 0)}
-						{renderEst(t('voyage.estimate.1_refill'), refill++, 1)}
-						{renderEst(t('voyage.estimate.n_refills', { n: '2' }), refill++, 2)}
-					</tbody></Table>
-					<p>
-						{t('voyage.estimate.voyage_long_advice', {
-							h: `${this.config?.selectedTime ?? 20}`,
-							r: `${estimate['refillshr20']}`,
-							d: `${estimate['dilhr20']}`,
-							re: `${estimate['refillshr20']}`
-						})}
-					</p>
-					{estimate.final && this._renderChart()}
-				</div>
-			);
-		}
-	}
-
-	_renderRewardsTitle(rewards): JSX.Element {
-		const { roster } = this.props;
-		const { t, tfmt } = this.context.localized;
-		const crewGained = rewards.filter(r => r.type === 1);
-		const bestRarity = crewGained.length == 0 ? 0 : crewGained.map(c => c.rarity).reduce((acc, r) => Math.max(acc, r));
-		const bestCrewCount = crewGained
-			.filter(c => c.rarity == bestRarity)
-			.map(c => c.quantity)
-			.reduce((acc, c) => acc + c, 0);
-		const chronReward = rewards.filter(r => r.symbol === 'energy');
-		const chrons = chronReward.length == 0 ? 0 : chronReward[0].quantity;
-		const honorReward = rewards.filter(r => r.symbol === 'honor');
-		const honor = honorReward.length == 0 ? 0 : honorReward[0].quantity;
-		let h = 0;
-		if (roster?.length && crewGained?.length) {
-			let duplicates = crewGained.filter((crew) => roster.some((rost) => rost.symbol === crew.symbol && rost.rarity === rost.max_rarity)).map((cg) => roster.find(r => r.symbol === cg.symbol));
-			if (duplicates?.length) {
-				for (let crew of duplicates) {
-					if (crew.max_rarity === 5) {
-						h += 550;
-					}
-					else if (crew.max_rarity === 4) {
-						h += 200;
-					}
-					else if (crew.max_rarity === 3) {
-						h += 100;
-					}
-					else if (crew.max_rarity === 2) {
-						h += 50;
-					}
-					else {
-						h += 25;
-					}
-				}
-			}
-		}
-
-		const dupeHonor = h + honor;
-
-		return (
-			<span>
-				{`${t('base.rewards')}: ${bestCrewCount} ${bestRarity}* `}&nbsp;
-				{` ${chrons.toLocaleString()} `}
-				<img
-					src={`${process.env.GATSBY_ASSETS_URL}atlas/energy_icon.png`}
-					style={{width : '16px', verticalAlign: 'text-bottom'}}
-				/>&nbsp;&nbsp;
-				{` ${honor.toLocaleString()} `}
-				<img
-					src={`${process.env.GATSBY_ASSETS_URL}currency_honor_currency_0.png`}
-					style={{width : '16px', verticalAlign: 'text-bottom'}}
-				/>
-
-				{dupeHonor && (
-					<span> ({tfmt('voyage.or_n_h_if_dupes_dismissed', {
-						n: dupeHonor.toLocaleString(),
-						h: <img
-								src={`${process.env.GATSBY_ASSETS_URL}currency_honor_currency_0.png`}
-								style={{width : '16px', verticalAlign: 'text-bottom'}}
-							/>
-					})})
-					</span>
-				)}
-			</span>
-		)
-	}
-
-	_renderRewards(rewards): JSX.Element {
-		const { playerItems, roster } = this.props;
-
-		rewards = rewards.sort((a, b) => {
-			if (a.type == b.type && a.item_type === b.item_type && a.rarity == b.rarity)
-				return a.full_name.localeCompare(b.full_name);
-			else if (a.type == b.type && a.item_type === b.item_type)
-				return b.rarity - a.rarity;
-			else if (a.type == b.type)
-				return b.item_type - a.item_type;
-			else if (a.type == 2)
-				return 1;
-			else if (b.type == 2)
-				return -1;
-			return a.type - b.type;
-		});
-
-		const hideRarity = entry => entry.type == 3;
-		const rarity = entry => entry.type == 1 ? 1 : entry.rarity;
-		const getCrewSymbol = entry => entry.type == 1 ? entry.symbol : entry.symbol;
-		const assetURL = file => {
-			let url = file === 'energy_icon'
-				? 'atlas/energy_icon.png'
-				: `${file.substring(1).replaceAll('/', '_')}`;
-
-			if (!url.match(/\.png$/))
-				url += '.png'
-			return `${process.env.GATSBY_ASSETS_URL}${url}`;
-		};
-
-		const itemsOwned = item => {
-			const pItem = playerItems?.find(i => i.symbol == item.symbol);
-			return `(Have ${pItem ? (pItem?.quantity ?? 0) > 1000 ? `${Math.floor((pItem.quantity ?? 0)/1000)}k+` : pItem.quantity : 0})`;
-		};
-		const ownedFuncs = [
-			item => '',
-			item => {
-				const owned = roster?.filter(c => c.symbol == item.symbol);
-
-				for (const c of owned ?? [])
-					if (c.rarity < c.max_rarity)
-						return '(Fusable)';
-
-				return  (owned?.length ?? 0) > 0 ? '(Duplicate)' : '(Unowned)';
-			},
-			itemsOwned,
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			itemsOwned,	/* ship schematics */
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-			item => '',
-		];
-
-		var me = this;
-
-		return (
-			<>
-			<div>
-				<Grid columns={isMobile ? 2 : 5} centered padded>
-					{rewards.map((reward: Reward, idx) => {
-						checkReward(this.props.allItems ?? [], reward);
-						return (
-						<Grid.Column key={idx}>
-							<Header
-								style={{ display: 'flex' }}
-								icon={
-									<ItemDisplay
-										quantity={reward.quantity}
-										src={assetURL(reward.icon?.file)}
-										size={48}
-										rarity={rarity(reward)}
-										maxRarity={reward.rarity}
-										hideRarity={hideRarity(reward)}
-										targetGroup={reward.type === 1 ? 'voyageRewards_crew' : 'voyageRewards_item'}
-										itemSymbol={getCrewSymbol(reward)}
-										allCrew={this.props.allCrew}
-										allItems={this.props.allItems}
-										playerData={this.props.playerData}
-									/>
-								}
-								content={reward.name}
-								subheader={`Got ${reward.quantity?.toLocaleString()} ${ownedFuncs[reward.type] ? ownedFuncs[reward.type](reward) : reward.type}`}
-							/>
-						</Grid.Column>
-					)}
-				)}
-				</Grid>
-			</div>
-			</>
-		);
 	}
 
 	/* Not yet in use
@@ -665,13 +260,36 @@ export class VoyageStats extends Component<VoyageStatsProps, VoyageStatsState> {
 					<Accordion fluid exclusive={false}>
 					{
 						voyState !== 'recalled' && voyState !== 'completed' &&
-						accordionPanel(t('voyage.estimate.title'), this._renderEstimate(voyState === 'failed'), 'estimate', this._renderEstimateTitle())
+						accordionPanel(
+							t('voyage.estimate.title'),
+							<VoyageStatsEstimate
+								selectedTime={this.config?.selectedTime}
+								estimate={this.props.estimate ?? this.state.estimate}
+								needsRevive={voyState == 'failed'}
+ 							/>,
+							'estimate',
+							<VoyageStatsEstimateTitle
+								selectedTime={this.config.selectedTime}
+								estimate={this.props.estimate ?? this.state.estimate}
+							/>
+						)
 					}
 					{
 						accordionPanel(t('voyage.lineup.title'), this._renderCrew(), 'crew')
 					}
 					{
-						accordionPanel(t('base.rewards'), this._renderRewards(rewards), 'rewards', this._renderRewardsTitle(rewards))
+						accordionPanel(
+							t('base.rewards'),
+							<VoyageStatsRewards
+								roster={this.props.roster}
+								playerItems={this.props.playerItems}
+								rewards={rewards}
+							/>,
+							'rewards',
+							<VoyageStatsRewardsTitle
+								roster={this.props.roster}
+								rewards={rewards} />
+						)
 					}
 					</Accordion>
 				</div>
@@ -681,7 +299,17 @@ export class VoyageStats extends Component<VoyageStatsProps, VoyageStatsState> {
 				<div>
 					<Accordion fluid exclusive={false}>
 						{
-							accordionPanel(t('voyage.estimate.title'), this._renderEstimate(false), 'estimate', this._renderEstimateTitle())
+							accordionPanel(
+								t('voyage.estimate.title'),
+								<VoyageStatsEstimate
+									selectedTime={this.config?.selectedTime}
+									estimate={this.props.estimate ?? this.state.estimate}
+								/>,
+								'estimate',
+								<VoyageStatsEstimateTitle
+									selectedTime={this.config.selectedTime}
+									estimate={this.props.estimate ?? this.state.estimate} />
+							)
 						}
 						{
 							accordionPanel(t('voyage.lineup.title'), this._renderCrew(), 'crew')
