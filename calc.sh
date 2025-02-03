b37f04f0a27ce7c1e7136996e299f3f71e3f2621
Nathaniel Moschkin <ironywrit@gmail.com>
2025-02-03 18:26:31 -0500

    Remove deprecated scripts.

--- calc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm ./battle_run_cache.json
-unzip ../scripts/data/battle.zip
-npm run shipcalc
-node build/scripts/scoring
-zip ./battle.zip ./battle_run_cache.json && mv ./battle.zip ../scripts/data
--- recalc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npm run shipcalc-fresh
-node build/scripts/scoring
-zip ./battle.zip ./battle_run_cache.json && mv ./battle.zip ../scripts/data
