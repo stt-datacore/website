20be6051208cf7c4060f504b40c97392e4802c51
ussjohnjay <jtjasmin+stt@gmail.com>
2025-02-09 12:32:19 -0600

    Add 15% crit chance option (for bosses)

--- src/components/voyagecalculator/encounters/contestsimulator/contestant.tsx
+++ src/components/voyagecalculator/encounters/contestsimulator/contestant.tsx
@@ -31,6 +31,7 @@ export const Contestant = (props: ContestantProps) => {
 		{ key: '0%', value: 0, text: '0%' },
 		{ key: '5%', value: 5, text: '5%' },
 		{ key: '10%', value: 10, text: '10%' },
+		{ key: '15%', value: 15, text: '15%' },
 		{ key: '25%', value: 25, text: '25%' },
 		{ key: '45%', value: 45, text: '45%' },
 		{ key: '65%', value: 65, text: '65%' }
