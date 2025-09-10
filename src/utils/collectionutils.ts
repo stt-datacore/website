5f93303a76782b3722bd89740e88c7bbdc5b94df
Nathaniel Moschkin <ironywrit@gmail.com>
2025-09-10 12:57:44 -0400

    patch this.

--- src/utils/collectionutils.ts
+++ src/utils/collectionutils.ts
@@ -457,7 +457,7 @@ export function getAllCrewRewards(col: Collection) {
         return [];
     }
 
-    let rewards = JSON.parse(JSON.stringify(col.milestones)).map((c: Milestone) => c.rewards!).filter((f: Reward) => f.type === 1).flat() as Reward[];
+    let rewards = JSON.parse(JSON.stringify(col.milestones)).map((c: Milestone) => c.rewards!).flat().filter((f: Reward) => f.type === 1) as Reward[];
 
     rewards.sort((a, b) => a.symbol!.localeCompare(b.symbol!));
 
