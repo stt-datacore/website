54b0836a643d1d66056e25caedbe714c9fd9ae3b
Nathaniel Moschkin <ironywrit@gmail.com>
2025-09-14 18:41:40 -0400

    BTP Config Presets

--- src/components/optimizer/btpresets.tsx
+++ src/components/optimizer/btpresets.tsx
@@ -4,61 +4,52 @@ import { Modal, Button, Form, Input, Dropdown, Table, Message, Icon } from 'sema
 import { GlobalContext } from '../../context/globalcontext';
 import { BetaTachyonSettings } from '../../model/worker';
 import { DefaultBetaTachyonSettings } from './btsettings';
-
-export interface IPresetOption {
-	key: string;
-	name: string;
-	description: string;
-	category: string;
-	keywords: string[];
-	settings: BetaTachyonSettings;	
-    custom: boolean;
-	notes?: JSX.Element;
-};
-
-interface IPresetCategory {
-	name: string;
-	presets: IPresetOption[];
-}
+import { ITableConfigRow, SearchableTable } from '../searchabletable';
+import { OptionsPanelFlexRow } from '../stats/utils';
 
 type BetaTachyonPresetsProps = {
-    activeSettings?: BetaTachyonSettings;
+	presets: BetaTachyonSettings[];
+	setPresets: (value: BetaTachyonSettings[]) => void;
+    activeSettings: BetaTachyonSettings;
     setActiveSettings: (value: BetaTachyonSettings) => void;
 };
 
 export const BetaTachyonPresets = (props: BetaTachyonPresetsProps) => {
 	const globalContext = React.useContext(GlobalContext);
+	const { presets, setPresets, activeSettings: selectedPreset, setActiveSettings: setSelectedPreset } = props;
 
-	const [presets, setPresets] = React.useState<IPresetOption[]>([] as IPresetOption[]);
-	const [categories, setCategories] = React.useState<IPresetCategory[]>([] as IPresetCategory[]);
-	const [selectedPreset, setSelectedPreset] = React.useState<IPresetOption | undefined>(undefined);
+	// const [presets, setPresets] = React.useState<BetaTachyonSettings[]>([] as BetaTachyonSettings[]);
+	// const [categories, setCategories] = React.useState<IPresetCategory[]>([] as IPresetCategory[]);
+	// const [selectedPreset, setSelectedPreset] = React.useState<BetaTachyonSettings | undefined>(undefined);
 
-	if (!selectedPreset) return renderModal();
+	// if (!selectedPreset) return renderModal();
 
     React.useEffect(() => {
         renderPresets();
     }, [])
 
-	return (
-		<Message icon onDismiss={clearPreset}>
-			<Icon name='paint brush' color='blue' />
-			<Message.Content>
-				<Message.Header>
-					{selectedPreset.name}
-				</Message.Header>
-				{selectedPreset.description}
-				<div style={{ marginTop: '.5em' }}>
-					{renderModal()}
-				</div>
-			</Message.Content>
-		</Message>
-	);
+	return renderModal();
+
+	// return (
+	// 	<Message icon onDismiss={clearPreset}>
+	// 		<Icon name='paint brush' color='blue' />
+	// 		<Message.Content>
+	// 			<Message.Header>
+	// 				{/* {selectedPreset.name} */}
+	// 			</Message.Header>
+	// 			{/* {selectedPreset.description} */}
+	// 			<div style={{ marginTop: '.5em' }}>
+	// 				{renderModal()}
+	// 			</div>
+	// 		</Message.Content>
+	// 	</Message>
+	// );
 
 	function renderModal(): JSX.Element {
 		return (
 			<BetaTachyonPresetPicker
 				presets={presets}
-				categories={categories}
+				setPresets={setPresets}
 				selectedPreset={selectedPreset}
 				setSelectedPreset={setSelectedPreset}
 			/>
@@ -66,49 +57,26 @@ export const BetaTachyonPresets = (props: BetaTachyonPresetsProps) => {
 	}
 
 	function renderPresets(): void {
-		const presets = [] as IPresetOption[];
-		const customPresets = [
-			{
-				key: 'standard',
-				name: 'Standard Settings',
-				description: 'Default settings balance between crew utility and likelihood of retrieval',
-				keywords: ['standard'],
-				category: 'Standard',
-                settings: DefaultBetaTachyonSettings,
-                custom: false
-			}
-		] as IPresetOption[];
-
-		customPresets.forEach(custom => {
-			presets.push(custom);
-		});
-		
-		const categories = [ ... new Set(presets.map(c => c.category)) ].sort().map(name => {
-			return {
-				name,
-				presets: presets.filter(t => t.category === name)
-			} as IPresetCategory;
-		});
-
-		setCategories(categories);
 		setPresets([...presets]);
 	}
-	
-	function clearPreset(): void {		
-		setSelectedPreset(undefined);
+
+	function clearPreset(): void {
+		setSelectedPreset(DefaultBetaTachyonSettings);
 	}
 };
 
 type BetaTachyonPresetPickerProps = {
-	presets: IPresetOption[];
-	categories: IPresetCategory[];
-	selectedPreset: IPresetOption | undefined;
-	setSelectedPreset: (selectedTheme: IPresetOption | undefined) => void;
+	presets: BetaTachyonSettings[];
+	setPresets: (value: BetaTachyonSettings[]) => void;
+	selectedPreset: BetaTachyonSettings;
+	setSelectedPreset: (value: BetaTachyonSettings) => void;
 };
 
 const BetaTachyonPresetPicker = (props: BetaTachyonPresetPickerProps) => {
-    const { t, tfmt } = React.useContext(GlobalContext).localized;
-    const { presets, selectedPreset, setSelectedPreset, categories } = props;
+	const context = React.useContext(GlobalContext);
+    const { t, tfmt } = context.localized;
+	const { confirm } = context;
+    const { presets, selectedPreset, setSelectedPreset, setPresets } = props;
 
 	const [modalIsOpen, setModalIsOpen] = React.useState(false);
 
@@ -126,14 +94,20 @@ const BetaTachyonPresetPicker = (props: BetaTachyonPresetPickerProps) => {
 			</Modal.Header>
 			<Modal.Content scrolling>
 				<p>{t('cite_opt.btp.settings_picker.heading')}</p>
-				{modalIsOpen && <PresetsTable presets={presets} categories={categories} selectPreset={onPresetSelected} />}
+				{modalIsOpen && (
+					<PresetsTable
+						presets={presets}
+						deletePreset={deletePreset}
+						selectedPreset={selectedPreset}
+						selectPreset={onPresetSelected}
+					/>)}
 			</Modal.Content>
 			<Modal.Actions>
-				{selectedPreset &&
-					<Button color='red' onClick={() => setSelectedPreset(undefined)}>
+				{/* {selectedPreset &&
+					<Button color='red' onClick={() => setSelectedPreset(DefaultBetaTachyonSettings)}>
 						{t('global.clear')}
 					</Button>
-				}
+				} */}
 				<Button onClick={() => setModalIsOpen(false)}>
 					{t('global.close')}
 				</Button>
@@ -141,37 +115,53 @@ const BetaTachyonPresetPicker = (props: BetaTachyonPresetPickerProps) => {
 		</Modal>
 	);
 
+	function deletePreset(name: string) {
+		let found = presets.find(f => f.name?.toLowerCase()?.trim() === name?.toLowerCase().trim());
+		if (found) {
+			confirm({
+				title: t('delete.title'),
+				message: t('delete.prompt_x', { x: found.name }),
+				onClose: (result) => {
+					if (result) {
+						let newpresets = presets.filter(f => f.name?.toLowerCase()?.trim() !== name?.toLowerCase().trim());
+						setPresets(newpresets);
+						if (selectedPreset?.name === found.name) {
+							setSelectedPreset(DefaultBetaTachyonSettings);
+						}
+					}
+				}
+			});
+		}
+	}
+
 	function renderTrigger(): JSX.Element {
 		if (!selectedPreset) return <Button icon='paint brush' content={t('cite_opt.btp.settings_picker.button_text')} />;
 		return (
-			<Button floated='right' content={t('cite_opt.btp.settings_picker.change_button_text')} />
+			<Button floated='right' content={t('global.edit_presets')} />
 		);
 	}
 
-	function onPresetSelected(theme: IPresetOption): void {
+	function onPresetSelected(theme: BetaTachyonSettings): void {
 		setSelectedPreset(theme);
 		setModalIsOpen(false);
 	}
 };
 
 type PresetsTableProps = {
-	presets: IPresetOption[];
-	categories: IPresetCategory[];
-	selectPreset: (theme: IPresetOption) => void;
+	presets: BetaTachyonSettings[];
+	selectedPreset: BetaTachyonSettings;
+	selectPreset: (value: BetaTachyonSettings) => void;
+	deletePreset: (name: string) => void;
 };
 
 const PresetsTable = (props: PresetsTableProps) => {
-	const [state, dispatch] = React.useReducer(reducer, {
-		data: props.presets.sort((a, b) => a.name.localeCompare(b.name)),
-		column: 'name',
-		direction: 'ascending'
-	});
-	const { data, column, direction } = state;
-	const { categories } = props;
-	
+	const globalContext = React.useContext(GlobalContext);
+	const { t } = globalContext.localized;
+	const { presets, selectedPreset, selectPreset, deletePreset } = props;
+
 	const [query, setQuery] = React.useState('');
-	const [highlightedPreset, setHighlightedPreset] = React.useState<IPresetOption | undefined>(undefined);
-	const [presetFilter, setPresetFilter] = React.useState<string>('custom-only');
+	const [highlightedPreset, setHighlightedPreset] = React.useState<BetaTachyonSettings | undefined>(undefined);
+	const [presetFilter, setPresetFilter] = React.useState<string>('none');
 	const [activeCategories, setActiveCategories] = React.useState<string[] | undefined>(undefined);
 
 	const presetFilterOptions = [
@@ -179,10 +169,6 @@ const PresetsTable = (props: PresetsTableProps) => {
 		{ key: 'built-in', value: 'built-in', text: 'Show only built-in presets' },
 		{ key: 'custom-only', value: 'custom-only', text: 'Show only custom presets' },
 	];
-
-	const themeCategoryOptions = categories.map(cat => ({
-		key: cat.name, value: cat.name, text: cat.name
-	}));
 
 	interface ICustomRow {
 		column: string;
@@ -192,35 +178,26 @@ const PresetsTable = (props: PresetsTableProps) => {
 	};
 
 	const tableConfig = [
-		{ column: 'name', title: 'Preset', align: 'left' },
-	] as ICustomRow[];
+		{ width: 1, column: 'name', title: t('global.name') },
+		{ width: 1, column: 'is_custom', title: t('cite_opt.btp.settings.is_custom') },
+		{ width: 2, column: '', title: t('menu.tools_title') },
+	] as ITableConfigRow[];
 
-	const filteredData = data as IPresetOption[];
-
-	const filteredCategories = categories
-		.filter(f => !activeCategories?.length || activeCategories.includes(f.name))
-		.map((cat) => ({
-		... cat,
-		themes: cat.presets.filter(f => filteredData.some(fs => fs.name === f.name))
-	}));
+	const data = React.useMemo(() => {
+		return presets.filter(f => {
+			if (presetFilter === 'none') return true;
+			if (presetFilter === 'built-in' && f.is_custom) return false;
+			if (presetFilter === 'custom-only' && !f.is_custom) return false;
+			return true;
+		});
+	}, [presets, presetFilter]);
 
 	return (
 		<React.Fragment>
 			<Form>
-				<Input fluid iconPosition='left'
-					placeholder='Search for themes by name or description...'
-					value={query}
-					onChange={(e, { value }) => setQuery(value)}
-				>
-					<input />
-					<Icon name='search' />
-					<Button icon onClick={() => setQuery('')}>
-						<Icon name='delete' />
-					</Button>
-				</Input>
 				<Form.Group inline style={{marginTop: "0.5em"}}>
 					<Form.Field
-						placeholder='Filter themes'
+						placeholder={t('global.presets')}
 						control={Dropdown}
 						clearable
 						selection
@@ -228,70 +205,26 @@ const PresetsTable = (props: PresetsTableProps) => {
 						value={presetFilter}
 						onChange={(e, { value }) => setPresetFilter(value as string)}
 					/>
-					<Form.Field
-						placeholder='Filter categories'
-						control={Dropdown}
-						clearable
-						selection
-						multiple
-						options={themeCategoryOptions}
-						value={activeCategories}
-						onChange={(e, { value }) => setActiveCategories(value as string[])}
-					/>
 				</Form.Group>
 			</Form>
-
-			{filteredCategories.map((cat) => (
-				<div style={{marginTop: '0.25em', marginBottom: '0.25em'}}>
-					<div className='ui header segment'>{cat.name}</div>
-					<Table sortable celled selectable striped>
-						<Table.Header>
-							<Table.Row>
-								{tableConfig.map((cell, idx) => (
-									<Table.HeaderCell key={idx}
-										textAlign={cell.align ?? 'center'}
-										sorted={column === cell.column ? direction : undefined}
-										onClick={() => dispatch({ type: 'CHANGE_SORT', column: cell.column, descendFirst: cell.descendFirst })}
-									>
-										{cell.title}
-									</Table.HeaderCell>
-								))}
-							</Table.Row>
-						</Table.Header>
-						<Table.Body>
-							{cat.themes.map(row => renderTableRow(row))}
-						</Table.Body>
-					</Table>	
-				</div>
-			))}
-
-			{/* <Table sortable celled selectable striped>
-				<Table.Header>
-					<Table.Row>
-						{tableConfig.map((cell, idx) => (
-							<Table.HeaderCell key={idx}
-								textAlign={cell.align ?? 'center'}
-								sorted={column === cell.column ? direction : undefined}
-								onClick={() => dispatch({ type: 'CHANGE_SORT', column: cell.column, descendFirst: cell.descendFirst })}
-							>
-								{cell.title}
-							</Table.HeaderCell>
-						))}
-					</Table.Row>
-				</Table.Header>
-				<Table.Body>
-					{filteredData.map(row => renderTableRow(row))}
-				</Table.Body>
-			</Table> */}
-			{filteredData.length === 0 && <p>No themes found.</p>}
+			<SearchableTable
+				tableStyle={{width: '100%'}}
+				noSearch={true}
+				data={data}
+				config={tableConfig}
+				renderTableRow={renderTableRow}
+				filterRow={filterRow}
+				/>
 		</React.Fragment>
 	);
-
-	function renderTableRow(row: IPresetOption): JSX.Element {
-		const isHighlighted = highlightedPreset?.key === row.key;
+	function filterRow(row, filter, options) {
+		return true;
+	}
+	function renderTableRow(row: BetaTachyonSettings): JSX.Element {
+		const isHighlighted = selectedPreset?.name === row.name;
+		const custom = row.is_custom ? t('global.yes') : t('global.no')
 		return (
-			<Table.Row key={row.key}
-				onClick={() => validatePreset(row)}
+			<Table.Row key={row.name}
 				active={isHighlighted}
 				style={{ cursor: 'pointer' }}
 			>
@@ -299,16 +232,29 @@ const PresetsTable = (props: PresetsTableProps) => {
 					<span style={{ fontWeight: 'bold', fontSize: '1.1em' }}>
 						{row.name}
 					</span>
-					<div>{row.description}</div>
-					{!isHighlighted && !!row.notes && <div style={{ marginTop: '.5em' }}>{row.notes}</div>}
-					{isHighlighted && row.notes && <Message error>{row.notes}</Message>}
 				</Table.Cell>
-
+				<Table.Cell>
+					{custom}
+				</Table.Cell>
+				<Table.Cell>
+					<div style={{...OptionsPanelFlexRow, gap: '1em'}}>
+						<Button
+							onClick={() => validatePreset(row)}
+						>
+							<Icon name='selected radio' />&nbsp;{t('global.apply')}
+						</Button>
+						{!!row.is_custom && <Button
+							onClick={() => deletePreset(row.name)}
+							>
+							<Icon name='trash' />&nbsp;{t('global.delete')}
+						</Button>}
+					</div>
+				</Table.Cell>
 			</Table.Row>
 		);
 	}
 
-	function validatePreset(theme: IPresetOption): void {
+	function validatePreset(theme: BetaTachyonSettings): void {
 		setHighlightedPreset(undefined);
 		props.selectPreset(theme);
 	}
@@ -341,8 +287,8 @@ const PresetsTable = (props: PresetsTableProps) => {
 		}
 	}
 
-	function sorter(data: IPresetOption[], column: string, direction: string): void {
-		const sortBy = (comps: ((a: IPresetOption, b: IPresetOption) => number)[]) => {
+	function sorter(data: BetaTachyonSettings[], column: string, direction: string): void {
+		const sortBy = (comps: ((a: BetaTachyonSettings, b: BetaTachyonSettings) => number)[]) => {
 			data.sort((a, b) => {
 				const tests = comps.slice();
 				let test = 0;
@@ -357,15 +303,15 @@ const PresetsTable = (props: PresetsTableProps) => {
 			return path.split('.').reduce((a, b) => (a || {b: 0})[b], obj);
 		};
 
-		const compareNumberColumn = (a: IPresetOption, b: IPresetOption) => {
+		const compareNumberColumn = (a: BetaTachyonSettings, b: BetaTachyonSettings) => {
 			if (direction === 'descending') return getValueFromPath(b, column) - getValueFromPath(a, column);
 			return getValueFromPath(a, column) - getValueFromPath(b, column);
 		};
-		const compareTextColumn = (a: IPresetOption, b: IPresetOption) => {
+		const compareTextColumn = (a: BetaTachyonSettings, b: BetaTachyonSettings) => {
 			if (direction === 'descending') return getValueFromPath(b, column).localeCompare(getValueFromPath(a, column));
 			return getValueFromPath(a, column).localeCompare(getValueFromPath(b, column));
 		};
-		const compareName = (a: IPresetOption, b: IPresetOption) => a.name.localeCompare(b.name);
+		const compareName = (a: BetaTachyonSettings, b: BetaTachyonSettings) => a.name.localeCompare(b.name);
 
 		if (column === 'name') {
 			sortBy([compareTextColumn]);
