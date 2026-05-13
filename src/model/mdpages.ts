c8e34d4eb6658327cb9521caa59a392faa209816
Nathaniel Moschkin <ironywrit@gmail.com>
2026-05-13 01:00:18 -0400

    Add symlink for dynamically generated markdown json.

---
+++ src/model/mdpages.ts
@@ -0,0 +1,14 @@
+
+
+export interface MarkdownRoot {
+    [key: string]: MarkdownEntry[];
+    announcements: MarkdownEntry[];
+    pages: MarkdownEntry[];
+}
+
+export interface MarkdownEntry {
+    [key:string]: string | undefined;
+    file: string;
+    name?: string;
+    title?: string;
+}
