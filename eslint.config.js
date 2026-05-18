22028c3dfe0dbc71bd4b63e17c947feb9c0e8fca
Nathaniel Moschkin <ironywrit@gmail.com>
2026-05-17 23:27:16 -0400

    restore english (how it got lost?!)

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,8 @@ export default defineConfig([
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      'react-hooks/exhaustive-deps': 'off'
+      'react-hooks/exhaustive-deps': 'off',
+      'react-refresh/only-export-components': 'off'
     }
   },
 ])
