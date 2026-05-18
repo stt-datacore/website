e1361e555231bbc9f3801ce1ed0d09302cf1e6ad
Nathaniel Moschkin <ironywrit@gmail.com>
2026-05-17 23:11:12 -0400

    Remove unused files Remove babel tweak eslint config to allow explicit any

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default defineConfig([
       'prefer-const': 'off',
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
       'react-hooks/exhaustive-deps': 'off'
     }
   },
